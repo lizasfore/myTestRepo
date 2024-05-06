8c5c47658e92b2173d4163bce5862b1dd3709425
lizasfore <elizaveta.19.bk@gmail.com>
2024-05-06 16:14:30 +0300

    test

--- tests/negativeMessaging.spec.ts
+++ tests/negativeMessaging.spec.ts
@@ -1,91 +1,91 @@
-// import {
-//   test,
-//   expect,
-//   BrowserContext,
-//   Page,
-// } from "@playwright/test";
-// import { MainPage } from "./pages/main.page.js";
-// import { LoginPage } from "./pages/login.page.js";
-// import { SuperBillViewPage } from "./pages/SB_view.page.js";
-// import { ApiRequests } from "../helpers/apiRequests.js";
-// import { CommFunc } from "../helpers/commonFunctions.js";
-// import messages from "../helpers/messages.json";
+import {
+  test,
+  expect,
+  BrowserContext,
+  Page,
+} from "@playwright/test";
+import { MainPage } from "./pages/main.page.js";
+import { LoginPage } from "./pages/login.page.js";
+import { SuperBillViewPage } from "./pages/SB_view.page.js";
+import { ApiRequests } from "../helpers/apiRequests.js";
+import { CommFunc } from "../helpers/commonFunctions.js";
+import messages from "../helpers/messages.json";
 
-// export let cookieAuth: string = "";
+export let cookieAuth: string = "";
 
-// let page: Page;
-// let context: BrowserContext;
-// let loginPage: LoginPage;
-// let mainPage: MainPage;
-// let sbViewPage: SuperBillViewPage;
-// let apiRequests: ApiRequests;
-// let commFunc: CommFunc;
+let page: Page;
+let context: BrowserContext;
+let loginPage: LoginPage;
+let mainPage: MainPage;
+let sbViewPage: SuperBillViewPage;
+let apiRequests: ApiRequests;
+let commFunc: CommFunc;
 
-// test.beforeAll(async ({ browser }) => {
-//   context = await browser.newContext();
-//   page = await context.newPage();
+test.beforeAll(async ({ browser }) => {
+  context = await browser.newContext();
+  page = await context.newPage();
 
-//   loginPage = new LoginPage(page);
-//   mainPage = new MainPage(page);
-//   sbViewPage = new SuperBillViewPage(page);
-//   apiRequests = new ApiRequests();
-//   commFunc = new CommFunc(page);
+  loginPage = new LoginPage(page);
+  mainPage = new MainPage(page);
+  sbViewPage = new SuperBillViewPage(page);
+  apiRequests = new ApiRequests();
+  commFunc = new CommFunc(page);
 
-//   await loginPage.navigateToLogin();
-//   await loginPage.emailFieldInput();
-//   await loginPage.passwordFieldInput();
-//   await loginPage.loginBtnClick();
+  await loginPage.navigateToLogin();
+  await loginPage.emailFieldInput();
+  await loginPage.passwordFieldInput();
+  await loginPage.loginBtnClick();
 
-//   const cookies = await context.cookies();
-//   const sessionCookie = cookies.find(
-//     async (c) => c.name === ".AspNetCore.eAffirmCookieAuth"
-//   );
-//   console.log(sessionCookie!.value);
-//   cookieAuth = sessionCookie!.value;
-// });
+  const cookies = await context.cookies();
+  const sessionCookie = cookies.find(
+    async (c) => c.name === ".AspNetCore.eAffirmCookieAuth"
+  );
+  console.log(sessionCookie!.value);
+  cookieAuth = sessionCookie!.value;
+});
 
-// test("Initiate SMS sequence and send SMS", async () => {
-//   await test.step("Search SuperBill", async () => {
-//     await mainPage.showMenuBtnClick();
-//     await mainPage.superBillsBtnClick();
-//     await mainPage.viewSuperBillsBtnClick();
-//     await sbViewPage.patientPhoneFieldInput();
-//     await sbViewPage.searchBtnClick();
-//     await expect(sbViewPage.searchResult).toBeVisible();
-//   });
+test("Initiate SMS sequence and send SMS", async () => {
+  await test.step("Search SuperBill", async () => {
+    await mainPage.showMenuBtnClick();
+    await mainPage.superBillsBtnClick();
+    await mainPage.viewSuperBillsBtnClick();
+    await sbViewPage.patientPhoneFieldInput();
+    await sbViewPage.searchBtnClick();
+    await expect(sbViewPage.searchResult).toBeVisible();
+  });
 
-//   await test.step("Reset SuperBill", async () => {
-//     await sbViewPage.resetBtnClick();
-//     await expect
-//       .poll(
-//         async () => {
-//           const response = await sbViewPage.getSequenceStatus();
-//           return response;
-//         },
-//         {
-//           intervals: [1000, 2000],
-//           timeout: 10000,
-//         }
-//       )
-//       .toContain("Open");
-//   });
+  // await test.step("Reset SuperBill", async () => {
+  //   await sbViewPage.resetBtnClick();
+  //   await expect
+  //     .poll(
+  //       async () => {
+  //         const response = await sbViewPage.getSequenceStatus();
+  //         return response;
+  //       },
+  //       {
+  //         intervals: [1000, 2000],
+  //         timeout: 10000,
+  //       }
+  //     )
+  //     .toContain("Open");
+  // });
 
-//   await test.step("Initiate sequence and send SMS", async () => {
-//     await sbViewPage.initiateSmsSequenceBtnClick();
-//     await expect
-//       .poll(
-//         async () => {
-//           const response = await sbViewPage.getSequenceStatus();
-//           return response;
-//         },
-//         {
-//           intervals: [1000, 2000],
-//           timeout: 10000,
-//         }
-//       )
-//       .toContain("InProgress");
-//   });
-// });
+  // await test.step("Initiate sequence and send SMS", async () => {
+  //   await sbViewPage.initiateSmsSequenceBtnClick();
+  //   await expect
+  //     .poll(
+  //       async () => {
+  //         const response = await sbViewPage.getSequenceStatus();
+  //         return response;
+  //       },
+  //       {
+  //         intervals: [1000, 2000],
+  //         timeout: 10000,
+  //       }
+  //     )
+  //     .toContain("InProgress");
+  // });
+});
 
 // test("Patient sends a message to the server 'hello'", async () => {
 //   await test.step("Get first SMS message from the sequence", async () => {
